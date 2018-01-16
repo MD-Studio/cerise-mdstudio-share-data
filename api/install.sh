b6e406dabce3b56bbf495ec04c92e96103791a9e
felipeZ <tifonzafel@gmail.com>
2018-01-16 12:07:03 +0100

    added general installation script

---
+++ api/install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+CERISE_API_FILES="$1"
+CERISE_SPECIALIZATION="$2"
+
+if [ -d "$CERISE_API_FILES/mdstudio/github/$CERISE_SPECIALIZATION" ] ; then
+    cd "$CERISE_API_FILES/mdstudio/github/$CERISE_SPECIALIZATION"
+    git pull
+else
+    mkdir -p "$CERISE_API_FILES/mdstudio/github"
+    cd "$CERISE_API_FILES/mdstudio/github"
+    git clone -b develop https://github.com/MD-Studio/$CERISE_SPECIALIZATION.git
+fi
+
+if [ ! -f "$CERISE_API_FILES/mdstudio/energies/getEnergies.py" ] ; then
+    SCRIPT="https://raw.githubusercontent.com/MD-Studio/MDStudio/master/components/lie_md/lie_md/scripts/getEnergies.py"
+    wget $SCRIPT -P "$CERISE_API_FILES/mdstudio/bin"
+fi
+
+if [ ! -d "$CERISE_API_FILES/miniconda" ] ; then
+    wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh -O miniconda.sh
+    bash miniconda.sh -b -p $CERISE_API_FILES/miniconda
+    conda config --set always_yes yes --set changeps1 no --set auto_update_conda False
+    source $CERISE_API_FILES/miniconda/bin/activate root
+    conda clean --index-cache
+    pip install panedr
+fi
