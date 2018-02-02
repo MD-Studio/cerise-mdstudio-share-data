6c0805ebc250a8e6aefe149c09e17cd9cdfbdf92
felipeZ <tifonzafel@gmail.com>
2018-02-02 17:55:28 +0100

    choose any python3 to run cwltiny

--- scripts/wrap_cwltiny.sh
+++ scripts/wrap_cwltiny.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-# load a python3 module                                                                                                                       
+# load a python3 module                                                                            \
+                                                                                                    
 function python_module  {
     # search for available python modules
     xs=$(module avail python 2>&1 | grep python)
     # split string into an array
     IFS=' ' read -r -a array <<<  $xs
-    # Iterate over the available modules
+    # Iterate over the available modules                                                            
     for x in ${array[*]}; do
-	# Select a python 3 module
+        # Select a python 3 module
         if [[ $x =~ ^python/3.[4-6]. ]]; then
             PYTHONMODULE=$x
             break
@@ -17,8 +18,24 @@ function python_module  {
     echo $PYTHONMODULE
 }
 
+function python_executable {
+    array=( "3" "3.4" "3.5" "3.6")
+    for x in ${array[*]}; do
+        var=$(command -v python${x})
+        if [[ ! -z $var ]]; then
+            echo $var
+            break
+        fi
+    done
+}
 
+# Load any python3 module avaialable
 PYTHONMODULE=$(python_module)
 module load $PYTHONMODULE
-python3 $HOME/.cerise/api/files/cerise/cwltiny.py $*
+echo "Loaded python module: $PYTHONMODULE"
+
+# Select a python 3 executable
+PYTHON3=$(python_executable)
+"Using: $PYTHON3"
+$PYTHON3 $HOME/.cerise/api/files/cerise/cwltiny.py $*
 
