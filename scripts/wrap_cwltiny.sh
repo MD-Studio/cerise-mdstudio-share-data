e77b873b29b4d60973e51aae030c6954328a62a0
felipeZ <tifonzafel@gmail.com>
2018-04-23 13:50:41 +0200

    generalized glob pattern for python

--- scripts/wrap_cwltiny.sh
+++ scripts/wrap_cwltiny.sh
@@ -3,13 +3,13 @@
 # load a python3 module                                                                                                                       
 function python_module  {
     # search for available python modules
-    xs=$(module avail python 2>&1 | grep python)
+    xs=$(module avail 2>&1 | grep python)
     # split string into an array
     IFS=' ' read -r -a array <<<  $xs
     # Iterate over the available modules
     for x in ${array[*]}; do
         # Select a python 3 module
-        if [[ $x =~ ^python/3.[4-6]. ]]; then
+        if [[ $x =~ python/3.[4-6]. ]]; then
             PYTHONMODULE=$x
             break
         fi
