0605962862d3677f089efe16253d5b39b50eaa08
felipeZ <tifonzafel@gmail.com>
2018-06-11 13:38:23 +0200

    added minimization steps

--- scripts/get_cwl_steps.sh
+++ scripts/get_cwl_steps.sh
@@ -2,7 +2,7 @@
 
 files=("decompose.cwl" "energies.cwl" "gromit.cwl" "it_minimization_1.cwl" "it_minimization_2.cwl")
 
-REPO="https://raw.githubusercontent.com/MD-Studio/cerise-mdstudio-share-data/master/api/steps/mdstudio"
+REPO="https://raw.githubusercontent.com/MD-Studio/cerise-mdstudio-share-data/ti/api/steps/mdstudio"
 
 for f in ${files[*]}; do
     wget $REPO/$f  -P "api/steps/mdstudio"
