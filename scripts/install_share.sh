a8d5876ae554e7e6d58bcac39d924b8ac6a0671a
felipe zapata <tifonzafel@gmail.com>
2018-09-28 11:18:17 +0200

    download getEnergies script from lie_md
    It was previously in the MDStudio main branch

--- scripts/install_share.sh
+++ scripts/install_share.sh
@@ -13,7 +13,7 @@ else
 fi
 
 if [ ! -f "$CERISE_API_FILES/mdstudio/bin/getEnergies.py" ] ; then
-    SCRIPT="https://raw.githubusercontent.com/MD-Studio/MDStudio/master/components/lie_md/lie_md/scripts/getEnergies.py"
+    SCRIPT="https://raw.githubusercontent.com/MD-Studio/lie_md/master/lie_md/scripts/getEnergies.py"
     wget $SCRIPT -P "$CERISE_API_FILES/mdstudio/bin"
 fi
 
