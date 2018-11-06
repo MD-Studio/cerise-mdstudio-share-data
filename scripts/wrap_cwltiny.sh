8166a0689b60dccabc44332a4bd3e142bf203b40
marcvdijk <marcvdijk@gmail.com>
2018-11-06 16:01:16 +0100

    Update wrap_cwltiny.sh
    Use 'cerise' python 3.6 miniconda environment instead of loading python 3 from modules

--- scripts/wrap_cwltiny.sh
+++ scripts/wrap_cwltiny.sh
@@ -1,40 +1,9 @@
 #!/bin/bash
 
-# load a python3 module                                                                                                                       
-function python_module  {
-    # search for available python modules
-    xs=$(module avail 2>&1 | grep python)
-    # split string into an array
-    IFS=' ' read -r -a array <<<  $xs
-    # Iterate over the available modules
-    for x in ${array[*]}; do
-        # Select a python 3 module
-        if [[ $x =~ python/3.[4-6]. ]]; then
-            PYTHONMODULE=$x
-            break
-        fi
-    done
-    echo $PYTHONMODULE
-}
-
-function python_executable {
-    array=( "3" "3.4" "3.5" "3.6")
-    for x in ${array[*]}; do
-        var=$(command -v python${x})
-        if [[ ! -z $var ]]; then
-            echo $var
-            break
-        fi
-    done
-}
-
-# Load any python3 module avaialablr
-#PYTHONMODULE=$(python_module)
-#module load $PYTHONMODULE
-# Select a python 3 executable
+# Run cwltiny.py using the 'cerise' python 3.6 miniconda env
+. $HOME/.cerise/api/files/miniconda/etc/profile.d/conda.sh
 conda activate cerise
 
-#PYTHON3=$(python_executable)
 python3.6 $HOME/.cerise/api/files/cerise/cwltiny.py $*
 
 conda deactivate
